0c9f39c6b3a524b1b68ddbb2a87d86fc577b2beb
Kyle Miller <kymille@microsoft.com>
2023-10-04 10:57:53 -0400

    Sleep for 10 min before calling data plane APIs

--- control-plane/experiencelab.sh
+++ control-plane/experiencelab.sh
@@ -582,6 +582,9 @@ function CreateFirstUser() {
   Verify $5 'CreateFirstUser-ERROR: Argument (DATA_PARTITION) not received'
   Verify $6 'CreateFirstUser-ERROR: Argument (FIRST_USER) not received'
 
+  echo "  Waiting 10 min for the instance to come up"
+  sleep 600
+
   ACCESS_TOKEN=$(curl \
       --request POST \
       --url https://login.microsoftonline.com/${2}/oauth2/token \
